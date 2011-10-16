e4a15962ba5a4a9084d7ac214f42d465db12471e
darkwisperer <darkwisperer@gmail.com>
2011-10-16 14:21:32 -0700

    initial commit

---
+++ DM_Sheet/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DM_Sheet
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {//File->print
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image
+            // assigned to Button2.
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "DM Sheet|*.DM";
+            saveFileDialog1.Title = "Save an DM Sheet";
+            saveFileDialog1.ShowDialog();
+
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "DM Sheet|*.DM";
+            openFileDialog1.Title = "Select a DM Sheet to load";
+            openFileDialog1.ShowDialog();
+        }
+    }
+}
