28ab605e415aa6bfc1203b01034a30d3369a4504
darkwisperer <darkwisperer@gmail.com>
2011-11-19 20:48:15 -0800

    added an about box
    Signed-off-by: darkwisperer <darkwisperer@gmail.com>

---
+++ DM_Sheet/ErrorMSG.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DM_Sheet
+{
+    partial class ErrorMSG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.Location = new System.Drawing.Point(107, 31);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(83, 13);
+            this.ErrorLabel.TabIndex = 0;
+            this.ErrorLabel.Text = "Error: something";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ErrorMSG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 112);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ErrorLabel);
+            this.Name = "ErrorMSG";
+            this.Text = "ErrorMSG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label ErrorLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DM_Sheet/ErrorMSG.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DM_Sheet
+{
+    public partial class ErrorMSG : Form
+    {
+        public ErrorMSG()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
