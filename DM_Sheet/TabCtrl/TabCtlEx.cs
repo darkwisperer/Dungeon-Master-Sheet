cef78ae63d664a2d68e6dae43f7254e9e16a4b6c
darkwisperer <darkwisperer@gmail.com>
2012-05-29 18:37:21 -0700

    Labeled each code file with there respective licenses. In the case of the CharTabPage I clearly stated which sections were to my knowledge under CPOL and which I created under GPL3. Added a CPOL page.
    Signed-off-by: darkwisperer <darkwisperer@gmail.com>

--- DM_Sheet/TabCtrl/TabCtlEx.cs
+++ DM_Sheet/TabCtrl/TabCtlEx.cs
@@ -1,3 +1,8 @@
+//---------------------------------------------------------------------------
+//This file is The Code Project Open License (CPOL) 1.02
+//http://www.codeproject.com/info/cpol10.aspx
+//---------------------------------------------------------------------------
+
 using System;
 using System.Collections;
 using System.ComponentModel;
